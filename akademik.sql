acdd63f8af3a3af75eb4427ad14a3f49ca676560
AdellaGhea <adella.ghea1@gmail.com>
2017-03-21 17:02:16 +0700

    Add files via upload

---
+++ akademik.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE akademik;
+USE akademik;
+CREATE TABLE mahasiswa(
+Nim Varchar(15) NOT NULL,
+Nama Varchar(150) NOT NULL,
+Alamat Varchar(150),
+Progdi Varchar(10);
+show tables;
+DESCRIBE mahasiswa;
+INSERT INTO mahasiswa VALUES ('1501530046','Adella Ghea Putri Charisma','Ngaliyan','Teknik Informatika'),
+('1501530040','Rudi Hariyanto','Kendal','Teknik Informatika'),
+('1501530032','Hendrik Nuryanto','Manyaran','Teknik Informatika');
+SELECT *FROM mahasiswa;
